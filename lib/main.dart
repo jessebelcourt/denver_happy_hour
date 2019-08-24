81aa0ebcfc275a95d64ae04343d8be0f80697ab9
Jesse Belcourt <jessebelcourt@jessebelcourt.hsd1.co.comcast.net>
2019-08-23 23:04:06 -0600

    Added in Google Maps, and Firebase Messaging for push notifications.

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
       routes: <String, WidgetBuilder>{
         '/': (BuildContext context) => HomeView(),
       },
--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeView extends StatefulWidget {
   HomeView();
@@ -7,17 +9,53 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        print('onMessage: $message');
+      },
+      onResume: (Map<String, dynamic> message) {
+        print('onResume: $message');
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        print('onLaunch: $message');
+      },
+    );
+    _firebaseMessaging.getToken().then((token) {
+      print(token);
+    });
+
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print('Settings registered: $settings');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Denver Happy Hour'),
       ),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            Text('more to come....'),
-          ],
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
         ),
       ),
     );
